66e71f63df18d606281eb803de95c951acf1c969
kdilot <kdilotdev@gmail.com>
2020-01-03 13:10:34 +0900

    Fingerprint

---
+++ src/screens/FingerprintScreen/styles.js
@@ -0,0 +1,50 @@
+export default {
+    container: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'red',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    contentContainer: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        width: '80%',
+        height: '50%',
+        backgroundColor: 'white',
+        zIndex: 10,
+    },
+    logo: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 5,
+    },
+    message: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+    },
+    description: error => ({
+        textAlign: 'center',
+        color: error ? 'red' : '#ADB0B2',
+        fontSize: 18,
+        marginHorizontal: 20,
+    }),
+    buttonContainer: {
+        flex: 2,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        marginBottom: 30,
+    },
+    buttonText: {
+        color: 'red',
+        fontSize: 15,
+        fontWeight: 'bold',
+    },
+};
