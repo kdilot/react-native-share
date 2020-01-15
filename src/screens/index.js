2d52a6f5998980b887aa7570f852393be68199d0
kdilot <kdilotdev@gmail.com>
2020-01-15 11:00:47 +0900

    index

---
+++ src/screens/index.js
@@ -0,0 +1,8 @@
+export { default as DefaultScreen } from './DefaultScreen';
+export { default as FingerprintScreen } from './FingerprintScreen';
+export { default as IconScreen } from './IconScreen';
+export { default as PinCodeScreen } from './PinCodeScreen';
+export { default as QrcodeScanner } from './QrcodeScreen/QrcodeScanner';
+export { default as QrcodeText } from './QrcodeScreen/QrcodeText';
+export { default as ScrollEventScreen } from './ScrollEventScreen';
+export { default as SkeletonLoadScreen } from './SkeletonLoadScreen';
